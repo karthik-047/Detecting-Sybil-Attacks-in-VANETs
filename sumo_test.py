74df0fdb197cbc2fa828e06196a234af64478c01
karthik-047 <keswar98@gmail.com>
2022-04-12 16:02:00 -0500

    Updated array to store infos

--- sumo_test.py
+++ sumo_test.py
@@ -2,14 +2,24 @@
 
 from __future__ import print_function
 from __future__ import absolute_import
+from dis import dis
+from gettext import find
 import os
 import sys
 import random
 import bisect
+import math
+import csv
+import numpy
+import pandas as pd
 import subprocess
 from collections import defaultdict
 import math
 import traci
+
+MAX_STEP = 1000
+NO_VEHICLES = 50
+RSU_COUNT = 10
 
 if 'SUMO_HOME' in os.environ:
     sys.path.append(os.path.join(os.environ['SUMO_HOME'], 'tools'))
@@ -19,20 +29,71 @@ import sumolib  # noqa
 from sumolib.miscutils import euclidean  # noqa
 from sumolib.geomhelper import naviDegree, minAngleDegreeDiff  # noqa
 
+
+#function to find the distance between two points
+def find_distance(p1,p2):
+    sq1 = (p2[0]-p1[0])** 2
+    sq2 = (p2[1]-p1[1])** 2
+    dis = math.sqrt(sq1+sq2)
+    return dis
+
+#list of RSU coordinates
+rsu_loc = [[2676.39,1667.14],[2866.53,1551.57],[3073.44,1622.41],[3261.72,1512.42],[3470.50,1586.99],[3540.96,1374.41],[3095.81,1385.66],[2844.16,1339.06],[2989.56,1857.28],[3379.16,1810.68]]
+
+
+#checking the distance between the RSUs
+dis_array = numpy.zeros(shape=(10,10))
+for i in range (0,len(rsu_loc)):
+    for j in range (0,len(rsu_loc)):
+        if(j!=i):
+            dis_array[i,j] = find_distance(rsu_loc[i],rsu_loc[j])
+
+#print(dis_array)  
+
+#starting the sumo simulation
 sumoCmd = ["sumo-gui.exe", "-c", "bryan_test.sumocfg"]
 traci.start(sumoCmd)
 step = 0
-traci.poi.add("POI1",2781.86,1766.33, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI2",2858.79,1451.28, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI3",3015.09,1634.45, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI4",3183.61,1507.45, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI5",3367.97,1691.96, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI6",3563.35,1484.36, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI7",3415.59,1264.55, color=(255, 255, 255), width=500, height=500, layer="1")
-traci.poi.add("POI8",2986.96,1353.70, color=(255, 255, 255), width=500, height=500, layer="1")
-print(traci.poi.getPosition("POI1"))
-while step < 2000:
+
+
+#adding Roadside units as Point of Interests (PoI)
+traci.poi.add("RSU1",2676.39,1667.14, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU2",2866.53,1551.57, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU3",3073.44,1622.41, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU4",3261.72,1512.42, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU5",3470.50,1586.99, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU6",3540.96,1374.41, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU7",3095.81,1385.66, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU8",2844.16,1339.06, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU9",2989.56,1857.28, color=(255, 255, 255), width=500, height=500, layer="1")
+traci.poi.add("RSU10",3379.16,1810.68, color=(255, 255, 255), width=500, height=500, layer="1")
+
+#veh_loc indexing: [step_value] [vehicle_id] [x and y coordinate]
+#storing x and y coordinates of 50 vehicles for MAX_STEP times
+veh_loc = numpy.zeros(shape=(MAX_STEP,NO_VEHICLES,2))
+
+#to store distance between all rsus and all vehicles
+dis_db = numpy.zeros(shape=(MAX_STEP,RSU_COUNT,NO_VEHICLES))
+#using step in simulation
+while step < MAX_STEP:
    traci.simulationStep()
-   #print(traci.vehicle.getPosition("0"))
+   id_count  = traci.vehicle.getIDCount()
+   id_names = traci.vehicle.getIDList()
+   #print(traci.vehicle.getPosition('0'),traci.vehicle.getPosition('1'))
+   for i in id_names:
+       veh_loc[step,int(i)]= traci.vehicle.getPosition(i)
+       #print(i,traci.vehicle.getPosition(i))
+   #print(veh_loc[step])
+   #print("\n")
    step = step+1
 traci.close()
+
+#calculating the distance between RSU and vehicles for 10RSU, 50vehicles, MAX_STEP times
+for k in range (MAX_STEP):
+    for i in range (RSU_COUNT):
+        for j in range (NO_VEHICLES):
+            if(any(veh_loc[k][j])!=0):
+                dis_db[k][i][j] = find_distance(rsu_loc[i],veh_loc[k][j])
+    
+#print(veh_loc)
+print(dis_db[step-1])
